ff58a7d68f863dacac7fc233874834555797e72b
Lucas Fogliarini Pedroso <lucas.pedroso@cogna.com.br>
2024-02-06 18:56:49 -0300

    traduzido para pt

--- NubankApp/Config.cs
+++ NubankApp/Config.cs
@@ -5,5 +5,6 @@
         public string? Login { get; set; }
 		public string? Password { get; set; }
 		public string? InvoiceClosingDay { get; set; }
+		public string? InvoiceClosingYear { get; set; }
 	}
 }
--- NubankApp/Program.cs
+++ NubankApp/Program.cs
@@ -4,10 +4,10 @@ using NubankApp;
 using NuClient;
 using NuClient.Models.Events;
 
-Console.WriteLine("Initing NubankApp using NuClient package ...");
-Console.WriteLine("Initing config (local.settings.json) ...");
+Console.WriteLine("Iniciando NubankApp usando o pacote 'NuCli' ...");
+Console.WriteLine("Iniciando as configurações (local.settings.json) ...");
 var config = GetConfig();
-Console.WriteLine($"Logging in NubankClient using '{config.Login}' and '{config.Password}'");
+Console.WriteLine($"Logando se no Nubank usando o Login: '{config.Login}' e Senha: '{config.Password}'");
 Console.WriteLine();
 
 var nubankClient = new NubankClient(config.Login, config.Password);
@@ -15,46 +15,45 @@ var result = await nubankClient.LoginAsync();
 
 if (result.NeedsDeviceAuthorization)
 {
-	Console.WriteLine("You must authenticate with your phone to be able to access your data.");
-	Console.WriteLine("Scan the QRCode below with you Nubank application on the following menu:");
-	Console.WriteLine("Nu(Seu Nome) > Perfil > Acesso pelo site");
+	Console.WriteLine("Você deve se autenticar com o aplicativo do Nubank para acessar os dados.");
+	Console.WriteLine("Entre no aplicativo Nubank > Perfil > Segurança > Acesso no Navegador");
+	Console.WriteLine("E scaneie o QRCode abaixo:");
 	Console.WriteLine();
-
 	Console.WriteLine(result.GetQrCodeAsAscii());
-	Console.WriteLine($"Use your phone to scan and after this press any key to continue...");
+	Console.WriteLine($"Depois de autorizado clique em qualquer tecla para continuar ...");
 	Console.ReadKey();
 
 	await nubankClient.AutenticateWithQrCodeAsync(result.Code);
 }
 
-
+Console.Clear();
 
 bool exit = false;
 while (!exit)
 {
-	Console.WriteLine("c: Credit Card transactions.");
-	Console.WriteLine("e: exit.");
+	Console.WriteLine("c: Transações de Cartão de Crédito.");
+	Console.WriteLine("e: Sair.");
 	var opt = Console.ReadKey();
 	Console.Clear();
 	switch (opt.Key)
 	{
 		case ConsoleKey.C:
-			int currentYear = DateTime.Now.Year;
+			int invoiceClosingYear = int.Parse(config.InvoiceClosingYear);
 			int invoiceClosingDay = int.Parse(config.InvoiceClosingDay);
-			Console.WriteLine("I need a closing date for a specific invoice to filter transactions.");
-			Console.WriteLine($"Invoice closing year: '{currentYear}' (current year)");
-			Console.WriteLine($"Invoice closing day: '{invoiceClosingDay}' (from configuration)");
-			Console.Write("Then choice the invoice closing month: ");
+			Console.WriteLine("Eu preciso de uma data da fatura para filtrar as transações.");
+			Console.WriteLine($"Ano de fechamento da fatura: '{invoiceClosingYear}' (from configuration)");
+			Console.WriteLine($"Dia de fechamento da fatura: '{invoiceClosingDay}' (from configuration)");
+			Console.Write("Por último preciso do mês de fechamento da fatura: ");
 			var monthKey = Console.ReadLine();
-			if (!int.TryParse(monthKey, out int month))
+			if (!int.TryParse(monthKey, out int invoiceClosingMonth))
 			{
-				Console.WriteLine("Month must be number!");
+				Console.WriteLine("Mês precisa ser um número!");
 				return;
 			}
-			Console.Write("Filter a card by typing the last 4 digits or any key for all cards: ");
+			Console.Write("É possível filtrar por um cartão digitando os últimos 4 dígitos ou digite qualquer tecla para todos os cartões: ");
 			var card = Console.ReadLine();
 
-			await GetTransactionsAsync(currentYear, month, invoiceClosingDay, card);
+			await GetTransactionsAsync(invoiceClosingYear, invoiceClosingMonth, invoiceClosingDay, card);
 			break;
 		case ConsoleKey.E:
 			exit = true;
@@ -68,16 +67,16 @@ while (!exit)
 async Task GetTransactionsAsync(int year, int month, int day, string? card = null)
 {
 	card = string.IsNullOrWhiteSpace(card) ? null : card;
-	var cardMessage = card == null ? "all cards" : $"final card '{card}'";
+	var cardMessage = card == null ? "todos os cartões" : $"o cartão com final '{card}'";
 	Console.Clear();
 	var to = new DateTime(year, month, day);
 	var from = to.AddMonths(-1);
-	Console.WriteLine($"Looking for transactions from '{from.ToShortDateString()}' to '{to.ToShortDateString()}' of {cardMessage}");
+	Console.WriteLine($"Procurando por transações de '{from.ToShortDateString()}' até '{to.ToShortDateString()}' com {cardMessage}");
 	var events = await nubankClient.GetEventsAsync();
 	events = events
 				.Where(e => e.Time >= from && e.Time < to)
 				.Where(e => e.Category == Event.transaction);
-	Console.WriteLine($"Found {events.Count()} Transactions.");
+	Console.WriteLine($"Encontrei {events.Count()} transações.");
 	var transactions = new List<Transaction>();
 	var eventsCount = events.Count();
 	for (int i = 0; i < eventsCount; i++)
@@ -86,7 +85,7 @@ async Task GetTransactionsAsync(int year, int month, int day, string? card = nul
 		if(card != null)
 		{
 			decimal transactionDetailProcess = (decimal)(i+1) / eventsCount * 100;
-			Console.WriteLine($"Looking for Transaction Detail '{e.Id}'. {transactionDetailProcess:F2}%");
+			Console.WriteLine($"Procurando por detalhes da transação '{e.Id}'. {transactionDetailProcess:F2}%");
 			var transactionDetail = await nubankClient.GetTransactionDetailsAsync(e);
 			if (transactionDetail?.Card_last_four_digits != card)
 				continue;
@@ -128,6 +127,7 @@ Config GetConfig()
 	{
 		Login = config["login"],
 		Password = config["password"],
-		InvoiceClosingDay = config["invoiceClosingDay"]
+		InvoiceClosingDay = config["invoiceClosingDay"],
+		InvoiceClosingYear = config["invoiceClosingYear"]
 	};
 }
